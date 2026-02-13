a2ad6fe774a80ea8c4d9f9576ef82e53b053c6ec
StanislavPashchenko <stanislav.pashchenko.94@gmail.com>
2026-02-13 16:29:44 +0200

    Fix static files with WhiteNoise

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'shop.middleware.ForceDefaultLanguageMiddleware',
@@ -187,7 +188,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
