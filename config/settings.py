3d3bdcbb02c9c0d665896cf37936a7ae4d59b0fb
StanislavPashchenko <stanislav.pashchenko.94@gmail.com>
2026-02-13 16:45:44 +0200

    Move static to root static directory

--- config/settings.py
+++ config/settings.py
@@ -190,6 +190,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = '/media/'
